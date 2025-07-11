3724af5b660874413d6f64ba7fba8762b1a276d8
Omkar mankape <125972788+OmkarMnkp@users.noreply.github.com>
2025-07-11 14:22:23 +0530

    Login.jsx

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -3,6 +3,7 @@ import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
+// login
 const Login = () => {
   const { login } = useAuth();
   const navigate = useNavigate();
